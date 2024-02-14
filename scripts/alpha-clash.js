477986321eb1b96facab06dffb928611c5c25d65
al-amin90 <ijesun30@gmail.com>
2024-02-15 01:21:48 +0600

    js code added

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,13 +1,61 @@
+function handleKeyboardButtonPress(event) {
+    const playerPress = event.key;
+    
+    if (playerPress === "Escape") {
+        gameOver();
+    }
+
+    const currentAlphaElement = document.getElementById("current-alphabet");
+    const currentAlpha = currentAlphaElement.innerText.toLowerCase();
+
+    if (playerPress === currentAlpha) {
+        const currentScore = getTextElementValueById("current-score");
+        const newScore = currentScore + 1;
+        setTextElementValueById("current-score", newScore)
+
+        removeBackgroundColorById(currentAlpha);
+        continueGame();
+    }
+    else {
+        const lifeScore = getTextElementValueById("life-score");
+        const newScore = lifeScore - 1;
+
+        setTextElementValueById("life-score", newScore)
+
+        if (newScore === 0) {
+            gameOver();
+        }
+    }
+}
+
+document.addEventListener("keyup", handleKeyboardButtonPress);
+
 function continueGame() {
     const alphabet = getARandomAlphabet();
     const currentAlphaElement = document.getElementById("current-alphabet");
     currentAlphaElement.innerText = alphabet;
+
     // document.getElementById(alphabet).style.backgroundColor = "#FFA500";
     setBackgroundColorById(alphabet)
 }
 
 function play() {
     hideElementById("home-screen");
+    hideElementById("final-score");
     showElementById("play-ground");
+
+    setTextElementValueById("life-score", 5)
+    setTextElementValueById("current-score", 0)
     continueGame();
+}
+
+function gameOver() {
+    hideElementById("play-ground");
+    showElementById("final-score");
+
+    const currentAlpha = getTextElementById("current-alphabet");
+    removeBackgroundColorById(currentAlpha)
+
+    const getScore = getTextElementValueById("current-score");
+    setTextElementValueById("game-score", getScore)
 }
--- scripts/utiliti.js
+++ scripts/utiliti.js
@@ -17,3 +17,23 @@ function setBackgroundColorById(elementId) {
     const element = document.getElementById(elementId);
     element.classList.add("bg-[#FFA500]");
 }
+
+function removeBackgroundColorById(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.remove("bg-[#FFA500]");
+}
+
+function getTextElementValueById(elementId) {
+    const elementValue = document.getElementById(elementId).innerText;
+    return parseInt(elementValue);
+}
+
+function getTextElementById(elementId) {
+    const element = document.getElementById(elementId);
+    return element.innerText;
+}
+
+function setTextElementValueById(elementId, value) {
+    const element = document.getElementById(elementId);
+    element.innerText = value;
+}
